e38fbd03ed88a87ef6f1b56f9a3694391e9fca08
RUKUNDO Prince <rukundoprince951@gmail.com>
2023-06-11 20:47:15 +0200

    finished creating the form, heading to the functionalities

---
+++ client/script.js
@@ -0,0 +1,12 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+  loadInterval = setInterval(() => {}, 300);
+}
