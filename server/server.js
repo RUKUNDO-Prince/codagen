89bd8abf404f0369c6ed0f9772b232e1cd6ffe9f
RUKUNDO Prince <rukundoprince951@gmail.com>
2023-06-12 06:21:27 +0200

    finished the openai api configuration

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
