dfb7fc8a6c0035e2323893e78d5af1ab5a3eed75
RUKUNDO Prince <rukundoprince951@gmail.com>
2023-06-12 06:40:01 +0200

    finished setting up some routes and getting the response from the api

--- server/server.js
+++ server/server.js
@@ -1,12 +1,45 @@
-import express from "express";
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import { Configuration, OpenAIApi } from "openai";
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config();
+dotenv.config()
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello Codex',
+    });
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.8,
+            presence_penalty:0,
+            // stop:["\"\"\""]
+        });
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error});
+    }
+});
