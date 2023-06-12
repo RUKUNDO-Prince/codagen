ebd65f5d076553611a96b090e50160e0af1b0466
RUKUNDO Prince <rukundoprince951@gmail.com>
2023-06-12 07:39:13 +0200

    finished the coding ai

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     // FETCH DATA FROM SERVER -> THE BOT'S RESPONSE
-    const response = await fetch('https://localhost:5000/', {
+    const response = await fetch('http://localhost:5000/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
--- server/server.js
+++ server/server.js
@@ -1,3 +1,54 @@
+// import express from 'express'
+// import * as dotenv from 'dotenv'
+// import cors from 'cors'
+// import { Configuration, OpenAIApi } from 'openai'
+
+// dotenv.config()
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// app.get('/', async (req, res) => {
+//     res.status(200).send({
+//         message: 'Hello Codex',
+//     });
+// });
+
+// app.post('/', async (req, res) => {
+//     try {
+//         const prompt = req.body.prompt;
+
+//         const response = await openai.createCompletion({
+//             model: "text-davinci-003",
+//             prompt: `${prompt}`,
+//             temperature: 0,
+//             max_tokens: 3000,
+//             top_p: 1,
+//             frequency_penalty: 0.8,
+//             presence_penalty:0,
+//             // stop:["\"\"\""]
+//         });
+//         res.status(200).send({
+//             bot: response.data.choices[0].text
+//         });
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).send({ error});
+//     }
+// });
+
+// app.listen(5000, () => {
+//     console.log('Server is running on port 5000');
+// });
+
+
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
@@ -11,39 +62,38 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 app.get('/', async (req, res) => {
-    res.status(200).send({
-        message: 'Hello Codex',
-    });
-});
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
 
 app.post('/', async (req, res) => {
-    try {
-        const prompt = req.body.prompt;
+  try {
+    const prompt = req.body.prompt;
 
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.8,
-            presence_penalty:0,
-            // stop:["\"\"\""]
-        });
-        res.status(200).send({
-            bot: response.data.choices[0].text
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ error});
-    }
-});
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
