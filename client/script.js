d816c72c45e80b46fe1551c8fb2978bb2fc43e44
RUKUNDO Prince <rukundoprince951@gmail.com>
2025-03-12 21:54:54 +0200

    some minor changes

--- client/script.js
+++ client/script.js
@@ -86,6 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
+    // const response = await fetch('https://codagen.onrender.com/', {
     const response = await fetch('https://codagen.onrender.com/', {
         method: 'POST',
         headers: {
